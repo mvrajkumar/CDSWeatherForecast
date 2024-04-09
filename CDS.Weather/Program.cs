c205592c207f9dca8c65c6ce523ee8d25f9cd419
Raj <55036133+mvrajkumar@users.noreply.github.com>
2024-04-09 16:52:25 +0100

    Additonal Tests

--- CDS.Weather/Program.cs
+++ CDS.Weather/Program.cs
@@ -1,3 +1,18 @@
+
+/* 
+ * 
+ * 
+ * 
+ * Original default boilder template code on issuing   
+ * 
+ * cd C:\Users\Administrator\source\repos
+ * md CentrelinkDataServicesWeather
+ * cd CentrelinkDataServicesWeather
+ * dotnet new sln
+ * dotnet new webapi -o CDS.Weather -f net8.0
+ * dotnet sln add CDS.Weather
+ * 
+ * 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -42,3 +57,50 @@ record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+*/
+
+using CDS.Weather.Wrappers;
+using AdamTibi.OpenWeather;
+using CDS.Weather;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<IClient>(_ => {
+    // Below is useful for load testing. It's undsireable to swamp the real API when load testing your app.
+    // So use the new ClientStub() object to avoid invoking too many calls to the real Web site. Instead use
+    // the local test environment. 
+    bool isLoad = bool.Parse(builder.Configuration["LoadTest:IsActive"]);
+    if (isLoad) return new ClientStub();
+    else
+    {
+        string apiKey = builder.Configuration["OpenWeather:Key"];
+        HttpClient httpClient = new HttpClient();
+        return new Client(apiKey, httpClient);
+    }
+});
+builder.Services.AddSingleton<INowWrapper>(_ => new NowWrapper());
+builder.Services.AddTransient<IRandomWrapper>(_ => new RandomWrapper());
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ CDS.Weather/README.txt
@@ -0,0 +1,24 @@
+rem
+rem How to create the solution and project from a cmd prompt.
+rem
+
+cd C:\Users\Administrator\source\repos
+md CentrelinkDataServicesWeather
+cd CentrelinkDataServicesWeather
+dotnet new sln
+dotnet new webapi -o CDS.Weather -f net8.0
+dotnet sln add CDS.Weather
+
+
+rem From VS Tools, NuGet Package Manager runn the command below.
+NuGet\Install-Package OpenWeatherMapSharp -Version 3.1.4
+rem from package manager browsing the ALL web NuGet repos for AdamTibi.OpenWeather package
+
+rem Now start to add the unit tests to the solution.
+dotnet new xunit -o CDS.Weather.Tests.Unit -f net8.0
+dotnet sln add CDS.Weather.Tests.Unit
+dotnet add CDS.Weather.Tests.Unit reference CDS.Weather
+rem run the single template empty test.
+dotnet test
+
+
